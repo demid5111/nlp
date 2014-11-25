c7bfc14e666b2462bf9b6738b41798a6c823709c
karpnv <karpnv@gmail.com>
2014-11-26 00:02:25 +0400

    Bug fix in collect_categories.py. Remove extra dependence. Add categories.list

--- collect_categories.py
+++ collect_categories.py
@@ -1,40 +1,57 @@
-from nlp_models import session,HseArticle
+# -*- coding: utf-8 -*-
+
+#from nlp_models import session,HseArticle
 import re
-from bs4 import UnicodeDammit
+#from bs4 import UnicodeDammit
 import shelve
-import pymorphy2
-from collect_info_for_lda import collectInfo
+#import pymorphy2
+#from collect_info_for_lda import collectInfo
 import codecs
-morph = pymorphy2.MorphAnalyzer()
+import MySQLdb as sql
+#morph = pymorphy2.MorphAnalyzer()
 
 # collectInfo()
-d = shelve.open('authors.list')
+d = shelve.open('categories.list')
 
-results = session.query(HseArticle).all();
+#results = session.query(HseArticle).all();
 total_categories = set([])
 
-for article in results:
- 
-  if article.elib != "":
-    elib = re.split(';|\s|,|\)|\(|"',article.elib)
+
+db = sql.connect(host="localhost", user="root", passwd="newpass", db="nlp", charset='utf8')
+cursor = db.cursor()
+sql = """SELECT interest, elib, keyword FROM hse_article"""
+cursor.execute(sql)
+data =  cursor.fetchall()
+#for rec in data:
+#    interest1, elib1, keyword1 = rec
+#    print interest1, elib1, keyword1
+db.close()
+
+for article in data:
+  elib2=article[1]
+  if elib2 != "":
+    elib = re.split(';|,',elib2)
     for i in elib:
       if i != '':
-        total_categories.add(morph.parse(UnicodeDammit(i.lower()).unicode_markup)[0].normal_form)
+        total_categories.add(i.lower().strip())
 
-  if article.interest != "":
-    interests = re.split(';|\s|,|\)|\(|"',article.interest)
+  interest2=article[0]
+  if interest2 != "":
+    interests = re.split(';|,',interest2)
     for i in interests:
       if i != '':
-        total_categories.add(morph.parse(UnicodeDammit(i.lower()).unicode_markup)[0].normal_form)
-
-  if article.keyword != "":
-    keywords = re.split(';|\s|,|\)|\(|"',article.keyword)
+        total_categories.add(i.lower().strip())
+  keyword2=article[2]
+  if keyword2 != "":
+    keywords = re.split(';|,|\)|\(|"',keyword2)
     for i in keywords:
         if i != '':
-          total_categories.append(morph.parse(UnicodeDammit(i.lower()).unicode_markup)[0].normal_form)
-        
+          try:
+            total_categories.add(i.lower().strip())
+          except:
+            pass
 for i in total_categories:
-  print UnicodeDammit(i.decode('koi8-r','ignore')).unicode_markup 
+  print i
 
 
 
